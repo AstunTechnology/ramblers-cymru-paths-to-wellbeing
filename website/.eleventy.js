5288a1601dddf686fccb5a7ae6985e1fc89dbacd
Matt Walker <walkermatt@longwayaround.org.uk>
2022-11-18 17:00:50 +0000

    Add header, basic styles

--- website/.eleventy.js
+++ website/.eleventy.js
@@ -2,6 +2,8 @@ const i18n = require('eleventy-plugin-i18n');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/static/css');
+  eleventyConfig.addPassthroughCopy('src/static/img');
+  eleventyConfig.addPassthroughCopy('src/static/fonts');
   eleventyConfig.addPassthroughCopy({ "../map/dist/*.css": "/static/css" });
   eleventyConfig.addPassthroughCopy({ "../map/dist/*.mjs*": "/static/js" });
 
@@ -10,6 +12,18 @@ module.exports = function (eleventyConfig) {
       "Paths to Wellbeing": {
         'en': 'Paths to Wellbeing',
         'cy': 'Llywbrau i Lesiant'
+      },
+      "Home": {
+        'en': 'Home',
+        'cy': 'Hafan'
+      },
+      "About": {
+        'en': 'About',
+        'cy': 'Ynghylch'
+      },
+      "Map": {
+        'en': 'Map',
+        'cy': 'Map'
       }
     },
     fallbackLocales: {
