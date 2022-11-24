54bd5349d1eaf17fda47f26b90d1236a24a9a2e9
tomchadwinAstun <tomchadwin@astuntechnology.com>
2022-11-24 13:59:33 +0000

    Abstract path colours

---
+++ map/src/config.js
@@ -0,0 +1,9 @@
+let difficultyColours = {
+  'Easy Access': 'rgba(35,200,35,1.0)',
+  'Leisurely': 'rgba(35,35,200,1.0)',
+  'Easy': 'rgba(35,200,200,1.0)',
+  'Moderate': 'rgba(200,200,35,1.0)',
+  'Strenuous': 'rgba(200,35,35,1.0)'
+}
+
+export { difficultyColours };
--- map/src/main.js
+++ map/src/main.js
@@ -16,6 +16,7 @@ import OlLayerSwitcher from 'ol-layerswitcher';
 import Popup from 'ol-popup';
 
 import { makeCommunitySource } from './community.js';
+import { difficultyColours } from './config.js';
 
 class PathsToWellbeingMap {
   constructor(options) {
@@ -37,28 +38,11 @@ class PathsToWellbeingMap {
         url: '/static/data/route_' + this.lang + '.geojson',
       }),
       style: function (feature, resolution) {
-        let color = 'rgba(0,0,0,1.0)';
-        switch (feature.get('difficulty')) {
-          case 'Easy Access':
-            color = 'rgba(35,200,35,1.0)';
-            break;
-          case 'Leisurely':
-            color = 'rgba(35,35,200,1.0)';
-            break;
-          case 'Easy':
-            color = 'rgba(35,200,200,1.0)';
-            break;
-          case 'Moderate':
-            color = 'rgba(200,200,35,1.0)';
-            break;
-          case 'Strenuous':
-            color = 'rgba(200,35,35,1.0)';
-            break;
-        }
+        console.log(difficultyColours[feature.get('difficulty')]);
         return new Style({
           stroke: new Stroke({
-            color: color,
-            width: 0,
+            color: difficultyColours[feature.get('difficulty')],
+            width: 2,
           }),
         });
       },
