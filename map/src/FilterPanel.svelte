b87514cf89a531d87a505a3b8b1dfd2cb8df28fa
tomchadwinAstun <tomchadwin@astuntechnology.com>
2022-12-02 12:14:26 +0000

    Filter radio button

--- map/src/FilterPanel.svelte
+++ map/src/FilterPanel.svelte
@@ -2,6 +2,11 @@
   import { createEventDispatcher } from "svelte";
 
   export let i18n = (key) => key;
+
+  const dispatch = createEventDispatcher();
+
+  let filters = ["Family-friendly", "Route difficulty"];
+  let selectedFilter = "familyFriendly";
 </script>
 
 <main class="panel-content filter-panel">
@@ -10,6 +15,13 @@
   </header>
   <form>
     <!-- Family Friendly Filter here... -->
+    {#each filters as filter}
+    <label>
+      <input type=radio bind:group={selectedFilter} value={filter} on:change="{() => dispatch("filterChange", { result: evt })}">
+      {filter}
+      <br>
+    </label>
+    {/each}
   </form>
   {@html i18n('Find Routes Instructions')}
 </main>
