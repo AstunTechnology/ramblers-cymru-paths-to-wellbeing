8460494678a565bb93bafacb61076a40beea27c6
tomchadwinAstun <tomchadwin@astuntechnology.com>
2022-12-02 17:13:14 +0000

    Style filter selection radio button group

--- map/src/FilterPanel.svelte
+++ map/src/FilterPanel.svelte
@@ -16,16 +16,10 @@
   </header>
   <form>
     <!-- Family Friendly Filter here... -->
-    <label>
-      <input type="radio" name="selectedFilter" value="Family-friendly" on:change={changeFilter} checked="true">
-      Family-friendly
-      <br>
-    </label>
-    <label>
-      <input type="radio" name="selectedFilter" value="Route difficulty" on:change={changeFilter}>
-      Route difficulty
-      <br>
-    </label>
+    <input type="radio" name="selectedFilter" id="familyFriendly" value="Family-friendly" on:change={changeFilter} checked="true">
+    <label for="familyFriendly">Family-friendly</label>
+    <input type="radio" name="selectedFilter" id="routeDifficulty" value="Route difficulty" on:change={changeFilter}>
+    <label for="routeDifficulty">Route difficulty</label>
   </form>
   {@html i18n('Find Routes Instructions')}
 </main>
