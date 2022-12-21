95b90cd6c809d633e79ff5f96676835a7af68981
tomchadwinAstun <tomchadwin@astuntechnology.com>
2022-12-21 17:11:04 +0000

    Add hover logic to family-friendly

--- map/src/main.js
+++ map/src/main.js
@@ -346,8 +346,7 @@ class PathsToWellbeingMap {
       this.hoverRouteUid.includes(feature.get('routeuid'))
     ) {
       mode = 'hover';
-    }
-    if (selected) {
+    } else if (selected) {
       mode = 'selected';
     } else if (this.selectedRoute) {
       mode = 'muted';
@@ -363,7 +362,7 @@ class PathsToWellbeingMap {
       return [start, this.routeOutlineStyle(mode), ...this.routeSelectedStyle, filterStyle];
     }
     if (mode == 'hover') {
-      return [start, this.routeOutlineStyle(mode), ...this.routeHighlightStyle, filterStyle];
+      return [start, this.routeOutlineStyle(mode), this.routeHighlightStyle(mode), filterStyle];
     }
     return [start, this.routeOutlineStyle(mode), filterStyle];
   }
@@ -426,27 +425,33 @@ class PathsToWellbeingMap {
 
   routeFamilyFriendlyStyle(feature, mode) {
     let opacity = mode === 'muted' ? 0.5 : 1;
-    let color = feature.get('family_friendly') ? '#3c3' : '#33c';
+    let color = `rgba(${
+      feature.get('family_friendly') ? '51,204,51' : '51,51,204'
+    },${opacity})`;
     let style = new Style({
       stroke: new Stroke({
         color: color,
         width: 5,
       }),
     });
-    if (mode === 'selected') {
+    if (mode = 'hover' || mode === 'selected') {
       style.setZIndex(1);
     }
     return style;
   }
 
-  routeHighlightStyle = [
-    new Style({
+  routeHighlightStyle(mode) {
+    let style = new Style({
       stroke: new Stroke({
         color: 'yellow',
         width: 11,
       }),
-    }),
-  ];
+    });
+    if (mode = 'hover' || mode === 'selected') {
+      style.setZIndex(1);
+    }
+    return style;
+  }
 
   routeSelectedStyle = [
     new Style({
