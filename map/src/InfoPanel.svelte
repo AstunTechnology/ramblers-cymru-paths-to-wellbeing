bbb1d44729380a8bb2e6be2a08f4960612f3b820
tomchadwinAstun <tomchadwin@astuntechnology.com>
2023-02-01 16:59:46 +0000

    Community in info panel

--- map/src/InfoPanel.svelte
+++ map/src/InfoPanel.svelte
@@ -47,18 +47,20 @@
 
 <div class="panel-content info-panel">
   <header>
-    <h1>{route.get("name")}</h1>
-    <button on:click={onCloseClick}
-      ><svg
-        width="20px"
-        height="20px"
-        viewBox="0 0 512 512"
-        xmlns="http://www.w3.org/2000/svg"
-        ><title>{i18n("Close route information")}</title><path
-          d="M289.94,256l95-95A24,24,0,0,0,351,127l-95,95-95-95A24,24,0,0,0,127,161l95,95-95,95A24,24,0,1,0,161,385l95-95,95,95A24,24,0,0,0,385,351Z"
-        /></svg
-      ></button
-    >
+    <p>{route.get("community")}</p>
+    <div>
+      <h1>{route.get("name")}</h1>
+      <button on:click={onCloseClick}
+        ><svg
+          width="20px"
+          height="20px"
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
+          ><title>{i18n("Close route information")}</title><path
+            d="M289.94,256l95-95A24,24,0,0,0,351,127l-95,95-95-95A24,24,0,0,0,127,161l95,95-95,95A24,24,0,1,0,161,385l95-95,95,95A24,24,0,0,0,385,351Z"
+          /></svg
+        ></button
+      ></div>
   </header>
   <table class="stats">
     <tr>
