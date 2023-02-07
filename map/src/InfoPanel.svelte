eb9f4fc50789848c806e5b3a194e6fff661eccaa
Joe-Marshall-Mills <120498006+Joe-Marshall-Mills@users.noreply.github.com>
2023-02-07 16:23:57 +0000

    Update InfoPanel.svelte

--- map/src/InfoPanel.svelte
+++ map/src/InfoPanel.svelte
@@ -77,7 +77,7 @@
     <tr>
       <th>{i18n("Difficulty")}:</th><td>{route.get("difficulty")}</td>
       <th>{i18n("Distance")}:</th><td
-        >{Math.round((route.get("length")}
+        >{Math.round(route.get("length")}
         {i18n("kilometers")}</td
       >
     </tr>
